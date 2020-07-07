e6c8dce8bb1b5a4d0f4034aa6e780f8ea77d7185
rushikantaria <rushikantaria@gmail.com>
2020-07-07 15:23:11 +0530

    file name and extention

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # to display file and directoru filders from currrent directory
@@ -8,7 +9,16 @@ echo Current Folder: $DIR
 echo
 echo List of Files:
 file *
-
+echo
+echo
+for item in *
+do
+	filename=$item
+	echo "$filename" | cut -f 1 -d '.'
+	echo "${filename##*.}"
+done
+echo
+echo
 for item in $(ls -1 *.txt)
 do
 	if [ -f $item ]
